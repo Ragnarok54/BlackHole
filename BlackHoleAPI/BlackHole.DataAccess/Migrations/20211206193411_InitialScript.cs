c48bb52d33a841309183e521e6118a130dc42d20
Bogdan Zanfir <bogdanzanfir23@gmail.com>
2021-12-06 22:39:49 +0200

    Add script migration

---
+++ BlackHoleAPI/BlackHole.DataAccess/Migrations/20211206193411_InitialScript.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.IO;
+
+namespace BlackHole.DataAccess.Migrations
+{
+    public partial class InitialScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Scripts", @"initialScript.sql");
+            var script = File.ReadAllText(sqlFile);
+
+            migrationBuilder.Sql(script);
+        }
+    }
+}
