f0bb4f6d00123eed205acfee2917b02d6da09953
Ragnarok54 <bogdanzanfir23@gmail.com>
2022-01-08 20:53:28 +0200

    Angular & Ionic version bump

--- BlackHoleFrontEnd/src/app/app.module.ts
+++ BlackHoleFrontEnd/src/app/app.module.ts
@@ -10,19 +10,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        IonicModule.forRoot(),
+        AppRoutingModule
     ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-    // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+        // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
