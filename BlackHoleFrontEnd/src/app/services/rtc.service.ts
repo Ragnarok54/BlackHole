c0428cae070275bb2a6b706548c99b7f2f3ef1df
Ragnarok54 <bogdanzanfir23@gmail.com>
2022-07-03 20:19:09 +0300

    Update STUN & TURN servers

--- BlackHoleFrontEnd/src/app/services/rtc.service.ts
+++ BlackHoleFrontEnd/src/app/services/rtc.service.ts
@@ -18,11 +18,23 @@ export class RtcService implements OnDestroy {
     config: {
       iceServers: [
         {
-          urls: [
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-          ],
-        }
+          urls: "stun:openrelay.metered.ca:80",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
       ]
     }
   };
